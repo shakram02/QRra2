f6e283a7a648289b266c7176343a80466c63d4fd
shakram02 <ahmedhamdyau@gmail.com>
2018-03-21 19:18:16 +0200

    start using espeak engine, bad pronounciation so far

--- app/src/main/java/shakram02/ahmed/qrra2/CharacterTracker.java
+++ app/src/main/java/shakram02/ahmed/qrra2/CharacterTracker.java
@@ -1,9 +1,8 @@
 package shakram02.ahmed.qrra2;
 
+import android.graphics.Rect;
 import android.speech.tts.TextToSpeech;
-import android.util.Log;
 
-import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.Tracker;
 import com.google.android.gms.vision.barcode.Barcode;
 
@@ -13,29 +12,9 @@ import com.google.android.gms.vision.barcode.Barcode;
 class CharacterTracker extends Tracker<Barcode> {
     private final TextToSpeech tts;
 
-    CharacterTracker(TextToSpeech tts) {
+    CharacterTracker(String value, Rect location, TextToSpeech tts) {
         this.tts = tts;
         tts.speak("Text to say aloud", TextToSpeech.QUEUE_ADD, null);
     }
 
-    @Override
-    public void onDone() {
-        super.onDone();
-    }
-
-    @Override
-    public void onNewItem(int id, Barcode item) {
-        Log.i("SpellerFactory", "ID:" + id + " Raw value:" + item.rawValue);
-        super.onNewItem(id, item);
-    }
-
-    @Override
-    public void onUpdate(Detector.Detections<Barcode> detections, Barcode item) {
-        super.onUpdate(detections, item);
-    }
-
-    @Override
-    public void onMissing(Detector.Detections<Barcode> detections) {
-        super.onMissing(detections);
-    }
 }
--- app/src/main/java/shakram02/ahmed/qrra2/QrSpellerFactory.java
+++ app/src/main/java/shakram02/ahmed/qrra2/QrSpellerFactory.java
@@ -2,6 +2,7 @@ package shakram02.ahmed.qrra2;
 
 import android.content.Context;
 import android.speech.tts.TextToSpeech;
+import android.util.Log;
 
 import com.google.android.gms.vision.MultiProcessor;
 import com.google.android.gms.vision.Tracker;
@@ -15,20 +16,38 @@ import java.util.Locale;
 
 public class QrSpellerFactory implements MultiProcessor.Factory<Barcode>, TextToSpeech.OnInitListener {
     TextToSpeech tts;
+    private static final String ESPEAK_ENGINE = "com.redzoc.ramees.tts.espeak";
+    private static final String ACTIVITY_TAG = "QrSpeller";
 
     public QrSpellerFactory(Context context) {
         tts = new TextToSpeech(context, this);
         tts.setLanguage(Locale.US);
+
+        tts = new TextToSpeech(context, this, ESPEAK_ENGINE);
     }
 
     @Override
     public Tracker<Barcode> create(Barcode barcode) {
-        return new CharacterTracker(tts);
+        // TODO extract barcode value and location,
+        // we shouldn't read in reverse order
+        // maybe we need to pause before finding a space character
+
+        // TODO if we want to track word location, we should return
+        // a better tracker
+        // We might want to join rectangles of many tracker also to track
+        // a whole word
+
+        tts.speak(barcode.rawValue, TextToSpeech.QUEUE_ADD, null);
+        return new Tracker<>();
     }
 
 
     @Override
     public void onInit(int status) {
-
+        if (status != TextToSpeech.SUCCESS) {
+            Log.e(ACTIVITY_TAG, "Initialization failed (status: $status).");
+        } else {
+            Log.i(ACTIVITY_TAG, "Initialization success (status: $status).");
+        }
     }
 }
